b9300c7972b3a47f2dd55ac38ce6994edbceb288
Mustafa Hassan <mustafaazeem@hotmail.com>
2025-09-04 17:09:31 +0200

    Lecture 02

--- 01 - Variables and Loops/Salary_calculator.py
+++ 01 - Variables and Loops/Salary_calculator.py
@@ -1,1 +1,18 @@
-# This is the Salary calculator program
+# '''This program helps you calculate 
+# your salary depending upon your 
+# current tax rate '''
+
+
+
+for i in range(5):
+
+    gross_salary = int( input(f'Please enter your salary for person {i}: ') )   # int (100,000)
+    tax_rate = int(input('enter your tax rate: '))
+
+    tax_amount = gross_salary * tax_rate / 100 
+    net_salary = gross_salary - tax_amount
+
+    print("your tax amount is ", tax_amount)
+    print("your net salary is ", net_salary)
+
+print("After loop")
