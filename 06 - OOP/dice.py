ead751acc9a74e8c0666ae0976d249250ce24dec
Mustafa Hassan <mustafaazeem@hotmail.com>
2025-10-02 17:19:13 +0200

    Lecture on objects and graphics

--- 06 - OOP/dice.py
+++ 06 - OOP/dice.py
@@ -1,14 +1,30 @@
 from random import randint 
 
+
 class Dice:
-    def __init__(self, sides, c):
+    faces = ['\u2680', '\u2681', '\u2682', '\u2683', '\u2684', '\u2685']
+        
+    def __init__(self, sides=6, color='white'):
         self.sides = sides
-        self.color = c 
+        self.color = color 
         self.face = 0
 
-    def get_face(self):
-        return self.face
+    def get_face(self):        
+        return __class__.faces[self.face-1]
 
     def roll(self):
         self.face = randint(1, self.sides)
+    
+    def __add__(self, other):
+        if self.get_face() == other.get_face():
+            return self.get_face() + other.get_face()
+        return 0
+    
+    def __str__(self):
+        return f'I have a dice with {self.sides} sides'
+    
+    def __repr__(self):
+        return f'This dice has {self.sides} sides with {self.color} color'
+
+
 
--- 06 - OOP/roll_dice.py
+++ 06 - OOP/roll_dice.py
@@ -1,14 +1,41 @@
 from dice import Dice 
+from graphics import *
+
 
 def main():
-    d1 = Dice(8, 'white')
-    d1.roll()
-    f = d1.get_face()
-    print(f)
-    d2 = Dice(16, 'blue')
-    d2.roll() 
-    g = d2.get_face()
-    print(g)
+    win = GraphWin("TDA548's window", 600, 600)
+    win.setBackground('green')
+    win.setCoords(0, 0, 10, 10)
+    # cir = Circle( Point(40,40),  20)
+    # cir.draw(win)
+
+    d1 = Dice(6, 'white')    
+    d2 = Dice(6, 'blue')
+    for i in range(10):
+
+
+        d1.roll()
+        d2.roll()
+
+        d1_face = Text(Point(4,5), d1.get_face())
+        d2_face = Text(Point(6,5), d2.get_face())
+
+        d1_face.setSize(36)
+        d2_face.setSize(36)
+
+        d1_face.draw(win)
+        d2_face.draw(win)
+        win.getMouse()
+
+        d1_face.undraw()
+        d2_face.undraw()
+    
+    # d1_face = d1.get_face()
+    # d2_face = d2.get_face()   
+
+    win.getMouse()
+
+    print(d1_face, d2_face)
 
 
 if __name__ == '__main__':
